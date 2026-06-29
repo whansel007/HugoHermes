846530abac8e3a3a403f605bc705ba90d3aae279
William Hansel <w.hansel007@gmail.com>
2026-06-29 20:29:42 +0800

    Refactor: Folder cleanup

--- himalaya/himalaya_list.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/bash
-
-/usr/local/bin/himalaya \
-envelope list \
---config /root/.config/himalaya/config.toml \
---output json
--- himalaya/himalaya_mark_seen.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/bash
-
-/usr/local/bin/himalaya flag \
---config /root/.config/himalaya/config.toml \
-add "Seen" $1
