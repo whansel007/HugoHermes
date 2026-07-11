b6192230d4d2aff153454d85635b0b765b5beafa
William Hansel <w.hansel007@gmail.com>
2026-07-11 21:25:17 +0800

    Feat: Detect Conflicts

--- scripts/google/calendar_create.py
+++ scripts/google/calendar_create.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
 from scripts.google.google_api import get_service
+from scripts.google.calendar_list import list_events
 from datetime import datetime, timedelta
 import json
 
 EVENT_PATH = "/root/.hermes/scripts/google/event_details.json"
-
 def create_event():
     service = get_service()
     
@@ -23,21 +23,47 @@ def create_event():
     
     startDate = raw.get('startDate') or now.strftime('%Y-%m-%d')
     startTime = raw.get('startTime') or now.strftime('%H:%M')
+    startDateTime = f"{startDate}T{startTime}:00+08:00"
     
     endDate = raw.get('endDate') or (now + timedelta(hours=1)).strftime('%Y-%m-%d')
     endTime = raw.get('endTime') or (now + timedelta(hours=1)).strftime('%H:%M')
+    endDateTime = f"{endDate}T{endTime}:00+08:00"
     
-    location = raw.get('location') or "The Void"
+    location = raw.get('location') or "???"
 
-    description = raw.get('description') or "Nuthin here ~"
+    description = raw.get('description') or ""
     
+    # Conflict Check
+    conflicts = list_events(time_min=startDateTime, time_max=endDateTime)
+    if not conflicts:
+        print("No conflicts.")
+    else:
+        print(f"{len(conflicts)} conflict(s) detected!!!")
+        print("The conflicting events are:")
+        
+        for e in conflicts:
+            title = e.get("summary", "(u forgot to title this one >_>)")
+            start = e["start"].get("dateTime", e["start"].get("date"))
+            end = e["end"].get("dateTime", e["end"].get("date"))
+            print(f"{start} - {end} : {title}")
+        
+        y_n = input("Create anyway? [Y/N] : ")
+        while y_n not in ["Y", "N", "y", "n"]:
+            print("Please only enter 'Y' or 'N'")
+            y_n = input("Create anyway? [Y/N] : ")
+        
+        if y_n in ["N", "n"]:
+            print("Event not created.")
+            return
+        
+    # Event Creation
     event = {
         "summary": summary,
         "start": {
-            "dateTime": f"{startDate}T{startTime}:00+08:00"
+            "dateTime": startDateTime,
             },
         "end":{
-            "dateTime": f"{endDate}T{endTime}:00+08:00",
+            "dateTime": endDateTime,
         },
         "location": location,
         "description": description,
--- scripts/google/calendar_list.py
+++ scripts/google/calendar_list.py
@@ -2,19 +2,29 @@
 from google_api import get_service
 from datetime import datetime, timedelta
 
-def list_events(max_results=10, time_min=None):
+def list_events(max_results=10, time_min=None, time_max=None):
     service = get_service()
     
     now_raw = datetime.now() 
     now = now_raw.strftime('%Y-%m-%dT%H:%M:%S+08:00')
     
-    response = service.events().list(
-        calendarId="primary",
-        maxResults=max_results,
-        singleEvents=True,
-        orderBy="startTime",
-        timeMin=time_min or now,
-    ).execute()
+    if time_max:
+        response = service.events().list(
+            calendarId="primary",
+            maxResults=max_results,
+            singleEvents=True,
+            orderBy="startTime",
+            timeMin=time_min or now,
+            timeMax=time_max,
+        ).execute()
+    else:
+        response = service.events().list(
+            calendarId="primary",
+            maxResults=max_results,
+            singleEvents=True,
+            orderBy="startTime",
+            timeMin=time_min or now,
+        ).execute()
     
     events = response.get("items", [])
     
@@ -22,5 +32,7 @@ def list_events(max_results=10, time_min=None):
         title = e.get("summary", "(u forgot to title this one >_>)")
         start = e["start"].get("dateTime", e["start"].get("date"))
         print(f"{start} - {title}")
+    
+    return events
         
 list_events()
